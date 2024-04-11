c24072d704b8004c79b8bc3a8e83261ede755d99
Kyrylo <kbuchinskiy1@gmail.com>
2024-04-11 20:38:29 +0300

    add CRUD for user, login/logout/sign up

--- msgr_app/settings.py
+++ msgr_app/settings.py
@@ -37,10 +37,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "msngr",
     "crispy_forms",
-    "bootstrap4"
+    "crispy_bootstrap4",
+    "msngr",
+
 ]
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -128,3 +131,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = "/"
+
